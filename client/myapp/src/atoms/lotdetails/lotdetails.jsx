e074850979a67667575ea220d97eda84457f0110
Asna1303 <asnahamza333@gmail.com>
2024-02-05 16:24:00 +0900

    did the flexing of two components and made the enough spacing

--- client/myapp/src/atoms/lotdetails/lotdetails.jsx
+++ client/myapp/src/atoms/lotdetails/lotdetails.jsx
@@ -4,7 +4,7 @@ import Button from '@mui/material/Button';
 const lotdetails = () => {
   return (
     <div>
-        <Button variant="contained" sx={{backgroundColor:"#F7C35F",borderradius:"5",color:"black" ,'&:hover': {
+        <Button variant="contained" sx={{backgroundColor:"#F7C35F",borderradius:"5",color:"black" , marginBottom:"20px",'&:hover': {
             backgroundColor: "#F7C35F"
           },}}>Lot Details</Button>
     
